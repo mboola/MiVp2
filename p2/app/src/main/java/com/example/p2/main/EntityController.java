6643241c2433ca1b627948f0a339fd9c75d3714e
Marcel Povill <88982198+mboola@users.noreply.github.com>
2024-11-29 13:45:34 +0100

    Fixed minor error

--- p2/app/src/main/java/com/example/p2/main/EntityController.java
+++ p2/app/src/main/java/com/example/p2/main/EntityController.java
@@ -1,5 +1,10 @@
-package com.example.p2.entities;
+package com.example.p2.main;
 
+import com.example.p2.entities.IEntity;
+import com.example.p2.entities.IEntityFactory;
+import com.example.p2.entities.RandomEntityFactory;
+
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -24,11 +29,12 @@ public class EntityController {
         // Checks if new entities must be created
         entityFactory.generateEntities(entities);
 
-        // Updates all entities of entities
-        for (IEntity entity : entities)
-        {
-            if (entity.update())
-                entities.remove(entity);
+        Iterator<IEntity> iterator = entities.iterator();
+        while (iterator.hasNext()) {
+            IEntity entity = iterator.next();
+            if (entity.update()) {
+                iterator.remove();
+            }
         }
     }
     public void draw(GL10 gl)
